842a77a29f9a0ddd1363bd65e42a8b3ffa8899c1
Chan-yeol Park <chanyeol.park@gmail.com>
2025-03-27 23:31:11 +0900

    Add Ble Scanner

--- app/src/main/java/com/example/myble/MainActivity.kt
+++ app/src/main/java/com/example/myble/MainActivity.kt
@@ -1,32 +1,239 @@
 package com.example.myble
 
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothManager
+import android.bluetooth.le.ScanCallback
+import android.bluetooth.le.ScanResult
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
+import androidx.annotation.RequiresPermission
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.myble.ui.theme.MyBleTheme
 
 class MainActivity : ComponentActivity() {
+    private val bluetoothAdapter: BluetoothAdapter? by lazy {
+        val bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
+        bluetoothManager.adapter
+    }
+
+    private val isBluetoothEnabled: Boolean
+        get() = bluetoothAdapter?.isEnabled == true
+
+    private val leScanCallback = object : ScanCallback() {
+        @RequiresPermission(android.Manifest.permission.BLUETOOTH_CONNECT)
+        override fun onScanResult(callbackType: Int, result: ScanResult) {
+            super.onScanResult(callbackType, result)
+            val device = result.device
+
+            if (!scannedDevices.contains(device)) {
+                scannedDevices.add(device)
+                Log.d(TAG, "Device found: ${device.name ?: "Unnamed device"} - ${device.address}")
+            }
+        }
+    }
+
+    // Bluetooth 활성화 요청 결과 처리
+    private val enableBluetoothRequest = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == RESULT_OK) {
+            // Bluetooth 활성화 성공
+            Log.d(TAG, "Bluetooth enabled")
+        } else {
+            // Bluetooth 활성화 실패
+            Log.d(TAG, "Failed to enable Bluetooth")
+        }
+    }
+
+    // 권한 확인 및 요청 함수
+    private fun checkAndRequestPermissions() {
+        val notGrantedPermissions = requiredPermissions.filter {
+            ContextCompat.checkSelfPermission(this, it) != PackageManager.PERMISSION_GRANTED
+        }.toTypedArray()
+
+        if (notGrantedPermissions.isNotEmpty()) {
+            multiplePermissionRequestForBle.launch(notGrantedPermissions)
+        } else {
+            Log.d(TAG, "All required permissions already granted")
+            // 모든 권한이 이미 있는 경우 필요한 작업 수행 (예: 스캔 시작)
+        }
+    }
+
+    // 복수 권한 요청 결과 처리
+    private val multiplePermissionRequestForBle = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { permissions ->
+        permissions.entries.forEach { (permission, isGranted) ->
+            if (isGranted) {
+                Log.d(TAG, "$permission granted")
+                // 권한 획득 후 필요한 작업 수행 (예: 스캔 시작)
+            } else {
+                Log.d(TAG, "$permission denied")
+                // 사용자에게 권한 필요성 알림 및 설정 화면으로 유도
+            }
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.S)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        Log.d(TAG, "onCreate")
+
+        // 필요한 권한 확인 및 요청
+        checkAndRequestPermissions()
+
         enableEdgeToEdge()
         setContent {
             MyBleTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    var scanning by remember { mutableStateOf(false) }
+
+                    Scaffold(
+                        modifier = Modifier.fillMaxSize()
+                    ) { innerPadding ->
+                        Column(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(innerPadding),
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            Spacer(modifier = Modifier.height(16.dp))
+
+                            // Bluetooth 활성화 버튼
+                            Button(onClick = {
+                                if (!isBluetoothEnabled) {
+                                    val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+                                    enableBluetoothRequest.launch(enableBtIntent)
+                                } else {
+                                    Log.d(TAG, "Bluetooth is already enabled")
+                                }
+                            }) {
+                                Text("Enable Bluetooth")
+                            }
+
+                            Spacer(modifier = Modifier.height(16.dp))
+
+                            // BLE 스캔 시작/중지 버튼
+                            Button(onClick = {
+                                if (isBluetoothEnabled) {
+                                    scanning = !scanning
+                                    if (scanning) {
+                                        scannedDevices.clear()
+                                        startBleScan()
+                                    } else {
+                                        stopBleScan()
+                                    }
+                                } else {
+                                    Log.d(TAG, "Bluetooth is not enabled")
+                                }
+                            }) {
+                                Text(if (scanning) "Stop BLE Scan" else "Start BLE Scan")
+                            }
+
+                            Spacer(modifier = Modifier.height(16.dp))
+
+                            // 스캔된 장치 목록 표시
+                            DeviceList(devices = scannedDevices)
+                        }
+                    }
                 }
             }
         }
+
+    }
+
+
+    @Composable
+    fun DeviceList(devices: List<BluetoothDevice>) {
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
+                .padding(16.dp)
+        ) {
+            items(devices) { device ->
+                Text(text = "${device.name ?: "Unnamed device"} - ${device.address}")
+            }
+        }
+    }
+
+    // BLE 스캔 시작
+    private fun startBleScan() {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.BLUETOOTH_SCAN) != PackageManager.PERMISSION_GRANTED) {
+            Log.d(TAG, "startBleScan: Permission BLUETOOTH_SCAN denied")
+            return
+        }
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+            Log.d(TAG, "startBleScan: Permission BLUETOOTH_CONNECT denied")
+            return
+        }
+        bluetoothAdapter?.bluetoothLeScanner?.startScan(leScanCallback)
+        Log.d(TAG, "BLE scan started")
+    }
+
+    // BLE 스캔 중지
+    private fun stopBleScan() {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.BLUETOOTH_SCAN) != PackageManager.PERMISSION_GRANTED) {
+            Log.d(TAG, "stopBleScan: Permission denied")
+            return
+        }
+        bluetoothAdapter?.bluetoothLeScanner?.stopScan(leScanCallback)
+        Log.d(TAG, "BLE scan stopped")
+    }
+
+    private var scannedDevices = mutableStateListOf<BluetoothDevice>()
+    // 필요한 권한 목록
+    private val requiredPermissions = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+        arrayOf(
+            android.Manifest.permission.BLUETOOTH_SCAN,
+            android.Manifest.permission.BLUETOOTH_CONNECT,
+            android.Manifest.permission.ACCESS_FINE_LOCATION
+        )
+    } else {
+        arrayOf(
+            android.Manifest.permission.ACCESS_FINE_LOCATION
+        )
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
     }
 }
 
